c432d9c210b467831cc36db93bd26c99c602c7ec
AnaLauCas <anacasadomoran@gmail.com>
2022-03-28 21:03:28 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require ('path')
+
+const app = express();
+
+const publicFolderPath = path.resolve (__dirname, './public')
+app.use (express.static(publicFolderPath))
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+ 
+app.get('/login', (req, res) =>{
+    res.sendFile(__dirname + '/views/login.html')
+})
